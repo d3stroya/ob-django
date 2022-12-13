89e26edb69a3aab7c8dae6cc30ae1ca63365da82
d3stroya <anabel.comino@gmail.com>
2022-12-13 10:01:09 +0100

    images fixed

--- wishlist/product/models.py
+++ wishlist/product/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     description = models.TextField(max_length=300, null=False, blank=True)
     price = models.FloatField()
     link = models.CharField(max_length=500, null=False, blank=False)
-    img = models.ImageField(upload_to='static/product_images', null=True, blank=True, default='https://cdn.pixabay.com/photo/2017/11/07/19/23/santa-claus-2927962_960_720.png')
+    img = models.CharField(max_length=300, null=False, blank=False, default='https://cdn.pixabay.com/photo/2017/11/14/00/57/christmas-2947257_960_720.jpg')
 
     def __str__(self):
         return self.title
